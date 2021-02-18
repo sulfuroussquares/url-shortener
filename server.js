436080de7002b7ef289850e206cb234e9900983c
Glitch (tar-curly-player) <none>
2021-02-18 00:19:45 +0000

    added ability to save long url to mongodb

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ require("dotenv").config();
 const express = require("express");
 // body-parser module required to read data from forms
 const bodyParser = require("body-parser");
+
+// mongoose required to connect to mongodb
+const mongoose = require("mongoose");
+// REMOVE THIS BEFORE PUBLISHING
+let mongo_uri =
+  "mongodb+srv://user1:X8qC23CPS1V4wxJe@cluster0.dsq4e.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongo_uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 const router = express.Router();
 const cors = require("cors");
 const app = express();
@@ -11,11 +22,19 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// mongodb schema
+// (it probably makes sense to create an app.js moving forward if I have this much going on unrelated to the server running)
+let urlSchema = new mongoose.Schema({
+  long_url: { type: String, required: true },
+  short_url: String
+});
+
+// a url is a single instance of a urlSchema; this declares the url model
+let url = mongoose.model("url", urlSchema);
+
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 // functionality
-// either we can create our own in-memory hash table for manipulating keys and retrieving their values
-// OR
 // we can have each submission generate a hash which corresponds to a mongodb _id
 // then when we want to retrieve that link, use the calculated hash value
 
@@ -24,10 +43,34 @@ app.post(
   "/api/shorturl/new",
   bodyParser.urlencoded({ extended: false }),
   (req, res, next) => {
-    console.log(req.body);
-    // res.send(req.body);
-    // res.json(req.body);
-    res.json({ original_url: req.body.url });
+    // retrieve data from page
+    let long_url = req.body.url;
+
+    // instantiate a url document(object)
+    let website = new url({ long_url: long_url });
+
+    // save document to database
+    // .save() is deprecated; here we use .insertOne()
+    // test is the name of the collection
+    website.save((err, site) => {
+      if (err) {
+        res.status(400).send("internal server error");
+      }
+      else {
+        res.status(200).json({
+      original_url: req.body.url,
+      short_url: "short url will go here"
+    });
+        
+      }
+    })
+   
+
+    // retrieve object id
+
+    // generate short url and save to database
+
+   
   }
 );
 
@@ -39,4 +82,5 @@ app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
 
-
+// example of creating a url instance
+// let website1 = new url({long_url: "https://google.com", short_url: })
